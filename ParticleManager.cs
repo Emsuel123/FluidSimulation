63b8babd3e2706fbeb355dfc71e924fb4f45268d
Emsuel123 <emu.gietenbruch@icloud.com>
2025-11-28 11:24:03 +0100

    Update ParticleManager.cs
    Übersetzung von dichte zu druck
    Bewegung durch Dichte und druck

--- ParticleManager.cs
+++ ParticleManager.cs
@@ -12,6 +12,12 @@ public class ParticleManager : MonoBehaviour
 
     [HideInInspector] public List<Sim> allePartikel = new List<Sim>();
     [HideInInspector] public List<float> dichteCache = new List<float>();
+    [HideInInspector] public List<Vector2> postionen = new List<Vector2>();
+    [HideInInspector] public List<Vector2> bewegung = new List<Vector2>();
+    [HideInInspector] public List<float> druckCache = new List<float>();
+
+    public float targetDichte;
+    public float druckMulti;
 
     void Awake()
     {
@@ -38,6 +44,9 @@ public class ParticleManager : MonoBehaviour
                     allePartikel.Add(s);
                     s.myIndex = allePartikel.Count - 1; 
                     dichteCache.Add(0f); // Platz für Partikel reservieren
+                    druckCache.Add(0f);
+                    postionen.Add(Vector2.zero);
+                    bewegung.Add(Vector2.zero);
                 }
             }
         }
@@ -51,19 +60,22 @@ public class ParticleManager : MonoBehaviour
             Debug.LogWarning($"[ParticleManager] pressureCache.Count ({dichteCache.Count}) != allePartikel.Count ({allePartikel.Count})");
         }
 
-        // Berechne Druck für jeden Partikel 
+        // Berechnen für jedes Partikel 
         for (int i = 0; i < allePartikel.Count; i++)
         {
-            dichteCache[i] = CalculatePressureFor(i);
+            dichteCache[i] = CalculateDichteFor(i);
+            druckCache[i] = ConvertDichteZuDruck(i);
+            postionen[i] = allePartikel[i].transform.position;
         }
 
+        
     }
 
-    //Druckberechnung für Partikel mit Index i
-    public float CalculatePressureFor(int i)
+    //Dichteberechnung für Partikel mit Index i
+    public float CalculateDichteFor(int i)
     {
         Sim self = allePartikel[i];
-        float pressure = 0f;
+        float Dichte = 0f;
 
         for (int j = 0; j < allePartikel.Count; j++)
         {
@@ -72,11 +84,48 @@ public class ParticleManager : MonoBehaviour
             float dist = Vector2.Distance(self.transform.position, other.transform.position);
             if (dist < self.influenceRadius)
             {
-                pressure += self.masse * SmoothingKernel(self.influenceRadius, dist);
+                Dichte += self.masse * SmoothingKernel(self.influenceRadius, dist);
             }
         }
 
-        return pressure;
+        return Dichte;
+    }
+
+    public float ConvertDichteZuDruck(int i)
+    {
+        float Druck = 0f;
+
+        for(i = 0;  i < allePartikel.Count; i++)
+        {
+            Druck += druckMulti * (targetDichte - dichteCache[i]);
+        }
+     
+        return Druck;
+    }
+
+    public Vector2 Bewegungberechen(Vector2 punkt)
+    {
+
+        Vector2 bewegungsVektor = Vector2.zero;
+        for(int i = 0; i < allePartikel.Count; i++)
+        {
+            float distanz = (postionen[i] - punkt).magnitude;
+            if(distanz == 0)
+            {
+                distanz += 0.01f;
+            }
+            Vector2 dir = (postionen[i] - punkt) / distanz;
+            if (dichteCache[i] == 0)
+            {
+                dichteCache[i] += 0.1f;
+            }
+            bewegungsVektor += druckCache[i] * dir * allePartikel[i].masse * SmoothingKernelAbleitung(allePartikel[i].influenceRadius, distanz) / dichteCache[i];
+
+        }
+
+
+
+        return bewegungsVektor;
     }
 
     // Extern zugänglich
@@ -85,5 +134,11 @@ public class ParticleManager : MonoBehaviour
         float volumen = Mathf.PI * Mathf.Pow(radius, 4) / 2;
         float wert = Mathf.Max(0f, radius - dist);
         return wert * wert * wert / volumen;
+    }
+
+    static float SmoothingKernelAbleitung(float radius, float dist)
+    {
+        float wert = Mathf.Max(0f, radius - dist);
+        return 3 * Mathf.Pow(wert, 2);
     }
 }
